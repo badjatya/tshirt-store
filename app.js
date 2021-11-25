1b634df6de09e36bdb34a6cdf7491e828490fbc9
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-25 15:44:51 +0530

    Configuring Express

--- app.js
+++ app.js
@@ -3,5 +3,23 @@ require("dotenv").config({ path: "./config/.env" });
 
 const app = express();
 
+// Library
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
+
+// Swagger API Documentation
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(fileUpload());
+app.use(morgan("tiny"));
+
 // Exporting app
 module.exports = app;
