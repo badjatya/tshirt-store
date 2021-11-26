8f6c74a9b3fc029bd2e6a244e74164b806e70dcd
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-26 13:22:38 +0530

    Home routes created

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,12 @@
-exports.home = (req, res) => {
+// Big Promise for removing the try Catch
+const BigPromise = require("../middlewares/bigPromise");
+
+exports.home = BigPromise(async (req, res) => {
   res.json({
     status: "success",
     greeting: "Hello from T-Shirt Store API",
   });
-};
+});
 
 exports.dummy = (req, res) => {
   res.json({
