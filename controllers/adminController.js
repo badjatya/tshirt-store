cb1071aad82accddf7abf4c1e9434c2239e46ffd
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-28 11:28:26 +0530

    Getting single user

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,12 +5,21 @@ const User = require("../models/user");
 const BigPromise = require("../middlewares/bigPromise");
 const CustomError = require("../utils/customError");
 
-exports.adminAllUsers = BigPromise(async (req, res, next) => {
+exports.allUsers = BigPromise(async (req, res, next) => {
   const users = await User.find({}).select("-password");
 
   res.status(200).json({
     status: "success",
     results: users.length,
     users,
+  });
+});
+
+exports.singleUser = BigPromise(async (req, res, next) => {
+  const user = await User.findById(req.params.id).select("-password");
+
+  res.status(200).json({
+    status: "success",
+    user,
   });
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 
 // Controllers
-const { adminAllUsers } = require("../controllers/adminController");
+const { allUsers, singleUser } = require("../controllers/adminController");
 
 // Middleware
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
-router.route("/users").get(isLoggedIn, customRole("admin"), adminAllUsers);
+router.route("/users").get(isLoggedIn, customRole("admin"), allUsers);
+router.route("/users/:id").get(isLoggedIn, customRole("admin"), singleUser);
 
 module.exports = router;
