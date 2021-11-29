37627306fe59b0e9ad5479bbfe70f216b11cbe1f
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-29 20:10:45 +0530

    Search, pagination and filtering util created

---
+++ utils/whereClause.js
@@ -0,0 +1,56 @@
+class WhereClause {
+  constructor(base, bigQ) {
+    this.base = base;
+    this.bigQ = bigQ;
+  }
+
+  search() {
+    const searchWord = this.bigQ.search
+      ? {
+          name: {
+            $regex: this.bigQ.search,
+            $options: "i",
+          },
+        }
+      : {};
+
+    this.base = this.base.find({ ...searchWord });
+    return this;
+  }
+
+  filter() {
+    const copyQ = { ...this.bigQ };
+
+    delete copyQ["search"];
+    delete copyQ["limit"];
+    delete copyQ["page"];
+
+    // Converting object to string
+    const stringCopyQ = JSON.stringify(copyQ);
+
+    stringOfCopyQ = stringOfCopyQ.replace(
+      /\b(gte|lte|gt|lt)\b/g,
+      (m) => `$${m}`
+    );
+
+    const jsonOfCopyQ = JSON.parse(stringOfCopyQ);
+
+    this.base = this.base.find(jsonOfCopyQ);
+    return this;
+  }
+
+  pager(resultPerPage) {
+    let currentPage = 1;
+
+    if (this.bigQ.page) {
+      currentPage = this.bigQ.page;
+    }
+
+    let skipValue = resultPerPage * (currentPage - 1);
+    this.base = this.base.limit(resultPerPage).skip(skipValue);
+
+    return this;
+  }
+}
+
+module.exports = WhereClause;
